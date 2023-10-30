fc5c313d0b8c1f383e19ca6648086c457a60644f
Maciej Wilewski <m.m.wilewski@gmail.com>
2023-10-30 12:17:48 +0100

    Dodanie formularza, poprawa obrazków i przycisków, zoom, interpunkcja i literówki

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Title change, after tab change
+
 
 const pageTitle = "Hewi Metal";
-const newTitle = "Niech Metal będzie z Tobą bracie";
+const newTitle = "Niech Metal będzie z Tobą";
 
 function handleVisibilityChange() {
     if (document.hidden) {
@@ -25,22 +25,19 @@ function menu(name){
 menu("ja")
 
 var bt1 = document.getElementById("dom");
-bt1.addEventListener("click", ()=>{
-    menu("ja")
-});
+bt1.addEventListener("click", ()=>{menu("ja")})
 var bt2 = document.getElementById("Avantasia");
 bt2.addEventListener("click", ()=>{menu("AvantasiaT")})
 var bt3 = document.getElementById("NK");
 bt3.addEventListener("click", ()=>{menu("NKT")})
 var bt4 = document.getElementById("BOM");
 bt4.addEventListener("click", ()=>{menu("BOMT")})
+var bt5 = document.getElementById("btkontakt");
+bt5.addEventListener("click", ()=>{menu("kontakt")})
 
 
 function filmy(name){
-    // var main =document.getElementsByClassName("yt");
-    // for(let m of main){
-    //     m.style.display="none";
-    // }
+
     var ele = document.getElementById(name)
     ele.style.display = "block";
 
@@ -72,23 +69,23 @@ play3.addEventListener("click", () => {
 var play4 = document.getElementById("play4");
 play4.addEventListener("click", () => {
     filmy("kaunaz");
-    play3.style.display = "none";
+    play4.style.display = "none";
 });
 
-// Function to start the video
+
 function startVideo(videoElement, playButton) {
     videoElement.play();
     console.log('Video is playing');
     playButton.style.display = "none";
 }
 
-// Function to stop the video
+
 function stopVideo(videoElement, playButton) {
     videoElement.pause();
     console.log('Video is stopped');
 }
 
-// Function to set up video controls
+
 function setupVideoControls(videoId, playButtonId, stopButtons) {
     var video = document.getElementById(videoId);
     var playButton = document.getElementById(playButtonId);
@@ -106,20 +103,18 @@ function setupVideoControls(videoId, playButtonId, stopButtons) {
 
     video.addEventListener('pause', function () {
         console.log('Video is paused');
-        // playButton.style.display = "block";
     });
 
     video.addEventListener('ended', function () {
         console.log('Video has ended');
-        // playButton.style.display = "block";
     });
 }
 
-// Set up video controls for the "power" video
-setupVideoControls('power', 'play1', ['Avantasia', 'NK', 'BOM']);
-setupVideoControls('scarecrow', 'play2', ['dom', 'NK', 'BOM']);
-setupVideoControls('kon', 'play3', ['dom', 'Avantasia', 'BOM']);
-setupVideoControls('kaunaz', 'play4', ['dom', 'Avantasia', 'NK']);
+
+setupVideoControls('power', 'play1', ['Avantasia', 'NK', 'BOM','btkontakt']);
+setupVideoControls('scarecrow', 'play2', ['dom', 'NK', 'BOM','btkontakt']);
+setupVideoControls('kon', 'play3', ['dom', 'Avantasia', 'BOM','btkontakt']);
+setupVideoControls('kaunaz', 'play4', ['dom', 'Avantasia', 'NK','btkontakt']);
 
 
 
